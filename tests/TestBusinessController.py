d94691d9c5315568d8a021b0982267c885c57f5e
andre Bland <andrebland123@gmail.com>
2022-09-08 14:20:05 -0700

    add: add testing to businesscontroller.py

--- tests/TestBusinessController.py
+++ tests/TestBusinessController.py
@@ -1,21 +1,31 @@
 import unittest
 import json
 import sys
+from unittest.mock import patch
+
 
 sys.path.append("..")
 
+from Course import Course
 from BusinessController import BusinessController
 from DatabaseController import DatabaseController
 
 
-testDBController = DatabaseController()
-testController = BusinessController(testDBController)
+
 
 class TestBusinessLogic(unittest.TestCase):
 
-    def test_createStudent(self):
-        testData = json.dumps({"name" : "andre"})
-        assert(type(testController.createStudent(testData)) == str)
+
+
+    def test_calculateCourseAverage(self):
+        with patch("BusinessController.AbstractDatabaseController.courseGetGrades") as mock:
+            testDBController = DatabaseController()
+            testController = BusinessController(testDBController)
+            mock.return_value = [5.0]
+            assert(testController.calculateCourseAverage(1) == 5)
+
+
+    #TODO expand testing
 
 
 if __name__ == '__main__':
